37f4a2802ccf1f4173222b1714bf45ccc325dc06
AndreiTS <andreitsteindorf@gmail.com>
2021-06-28 18:01:57 -0300

    try_from_into WIP

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -26,19 +26,40 @@ struct Color {
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = Box<dyn error::Error>;
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        match tuple {
+            (r, g, b) if r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255 => {
+                Err(Box::new("RGB must be between 0 and 255".into()))
+            },
+            (r, g, b) => {
+                Ok(Color {
+                    red: r as u8,
+                    green: r as u8,
+                    blue: b as u8,
+                })
+            }
+        }
+    }
 }
 
 // Array implementation
 impl TryFrom<[i16; 3]> for Color {
     type Error = Box<dyn error::Error>;
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        arr[..].try_into();
+    }
 }
 
 // Slice implementation
 impl TryFrom<&[i16]> for Color {
     type Error = Box<dyn error::Error>;
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err("slice must has 3 numbers");
+        }
+
+        (slice[0], slice[1], slice[2]).try_into()
+    }
 }
 
 fn main() {
