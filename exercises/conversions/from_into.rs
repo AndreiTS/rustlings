23a47583675596734e01089dff85e24aac216e54
AndreiTS <andreitsteindorf@gmail.com>
2021-06-28 18:01:37 -0300

    Update from_into.rs

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,35 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let count = s.matches(",").count();
+
+        if count != 1 {
+            return Person::default();
+        }
+
+        let s: Vec<&str> = s.split(',').collect();
+
+        let name = s[0].to_owned();
+
+        if name.len() == 0 {
+            return Person::default();
+        }
+
+        let age = s[1].parse::<usize>();
+
+        if age.is_err() {
+            return Person::default(); 
+        }
+        
+        let age = age.unwrap();
+
+        Person { name, age }
     }
 }
 
